1d3cb9adb9548cc7ae772b7bb0928011e10211b4
Ashutosh Gupta <ashutoshg479@gmail.com>
2024-01-06 16:07:17 +0530

    changed ui

--- src/views/UserSettings.vue
+++ src/views/UserSettings.vue
@@ -1,12 +1,43 @@
 <template>
   <div>
-    <button @click="userStore.clearAllData()">clear all saved data</button>
+    <!-- <button @click="userStore.clearAllData()">clear all saved data</button>
     <button @click="userStore.clearFlashCardProgress">
       clear flashcard progress
-    </button>
+    </button> -->
+    <div class="mb-3">
+  <button 
+    type="button" 
+    class="btn btn-sm btn-dark rounded-pill px-3 py-2 fw-bold" 
+    @click="userStore.clearAllData()"
+  >
+    Clear All Saved Data
+  </button>
+<span>{{ " " }}</span>
+  <button 
+    type="button" 
+    class="btn btn-sm btn-dark rounded-pill px-3 py-2 fw-bold" 
+    @click="userStore.clearFlashCardProgress"
+  >
+    Clear Flash Card Progress
+  </button>
+</div>
 
-    <p>API Password</p>
-    <input type="password" v-model="userStoreRef.userPrefs.value.password" />
+
+<div class="mb-3">
+  <label for="transparentInput" class="form-label">API Password:</label>
+  <div class="input-group" style="width: 60%;">
+    <input 
+      type="password" 
+      v-model="userStoreRef.userPrefs.value.password"
+      class="form-control bg-transparent border border-dark" 
+      id="transparentInput" 
+      placeholder="Type here..."
+    >
+  </div>
+</div>
+
+
+    
   </div>
 </template>
 
